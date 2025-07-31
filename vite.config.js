bc7e580b239950b00f773931725a7e7f37ac48b8
Christian <150170670+ThirdRevelation7@users.noreply.github.com>
2025-07-31 11:28:25 -0400

    Fixed config issues

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// });
-
-// vite.config.js
-export default {
+export default defineConfig({
   base: '/react-movie-watchlist/',
-};
+  plugins: [react()],
+});
+
